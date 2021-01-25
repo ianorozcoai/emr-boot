876209827caa5a5e52babd52c530fcab6fc9e204
Carlos Jhunie Pangilinan <carlos_jhunie.pangilinan@telus.com>
2021-01-24 22:37:28 -0500

    Fixed Profile link

--- src/main/java/com/cdsi/emr/personnel/Personnel.java
+++ src/main/java/com/cdsi/emr/personnel/Personnel.java
@@ -12,7 +12,6 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Size;
@@ -37,68 +36,67 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @Data public class Personnel extends Auditable implements UserDetails {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	@NotBlank(message = " is mandatory.")
-	private String username;
-	@NotBlank(message = " is mandatory.")
-	@JsonIgnore private String password;
-	@NotBlank(message = " is mandatory.")
-	private String firstName;
-	@NotBlank(message = " is mandatory.")
-	private String lastName;
-	@Size(min = 1, max = 1)
-	private String gender;
-	private String address;
-	private String contactNumber;
-	@Email(message = "Invalid email.")
-	private String email;
-	private String status = "ACTIVE";
-	private String userType = "DOCTOR";  // "DOCTOR" or "STAFF"
-	
-	private String credentials;
-	private String licenseNumber;
-	private String specialization;
-	private String ptrNumber;
-	private String sNumber;
-	@PastOrPresent
-	private LocalDate startDate = LocalDate.now();
-	@FutureOrPresent
-	private LocalDate endDate = LocalDate.of(9999, 12, 31);
-	
-	//@ElementCollection(fetch = FetchType.EAGER)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotBlank(message = " is mandatory.")
+    private String username;
+    @NotBlank(message = " is mandatory.")
+    @JsonIgnore private String password;
+    @NotBlank(message = " is mandatory.")
+    private String firstName;
+    @NotBlank(message = " is mandatory.")
+    private String lastName;
+    @Size(min = 1, max = 1)
+    private String gender;
+    private String address;
+    private String contactNumber;
+    @Email(message = "Invalid email.")
+    private String email;
+    private String status = "ACTIVE";
+    private String userType = "DOCTOR";  // "DOCTOR" or "STAFF"
+
+    private String credentials;
+    private String licenseNumber;
+    private String specialization;
+    private String ptrNumber;
+    private String sNumber;
+    @PastOrPresent
+    private LocalDate startDate = LocalDate.now();
+    private LocalDate endDate = LocalDate.of(9999, 12, 31);
+
+    //@ElementCollection(fetch = FetchType.EAGER)
     //private Collection<String> roles;
-	
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
-		//for (String role : roles) {
-		//    authorities.add(new SimpleGrantedAuthority(role));
-		//}
-		authorities.add(new SimpleGrantedAuthority(userType));
-		return authorities;
-	}
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
-	
-	public String getFullName() {
-		return this.firstName + " " + this.lastName;
-	}
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        //for (String role : roles) {
+        //    authorities.add(new SimpleGrantedAuthority(role));
+        //}
+        authorities.add(new SimpleGrantedAuthority(this.userType));
+        return authorities;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
 }
