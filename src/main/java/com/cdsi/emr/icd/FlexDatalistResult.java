d2c9d5ce09d1501896b1382555397bd797f1700b
Carlos Jhunie Pangilinan <carlos_jhunie.pangilinan@telus.com>
2021-03-03 09:47:09 -0500

    Fixed ICD datalist in Add Consultation modal.

---
+++ src/main/java/com/cdsi/emr/icd/FlexDatalistResult.java
@@ -0,0 +1,15 @@
+package com.cdsi.emr.icd;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FlexDatalistResult {
+
+    private List<Icd> results;
+}
--- src/main/java/com/cdsi/emr/icd/IcdController.java
+++ src/main/java/com/cdsi/emr/icd/IcdController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,51 +18,57 @@ import com.cdsi.emr.util.UXMessage;
 
 @Controller
 public class IcdController {
-	private IcdRepository icdRepository;
-	
-	public IcdController (
-		IcdRepository icdRepository
-	) {
-		this.icdRepository = icdRepository;
-	}
-	
-	@GetMapping("/icds")
-	public String listAll(Model model) {
-		List<Icd> icds = icdRepository.findAll();
-		model.addAttribute("icds", icds);
-		model.addAttribute(new Icd());
-		return "admin/icd_list";
-	}
-	
-	@GetMapping("/add_icd")
-	public String addIcdForm(Model model) {
-		model.addAttribute("icd", new Icd());
-		return "admin/add_icd_form";
-	}
-	
-	@GetMapping("edit_icd/{icdId}")
-	public String editIcd(
-			@PathVariable long icdId
-			,Model model
-			) {
-		Optional<Icd> oIcd = icdRepository.findById(icdId);
-		Icd icd = oIcd.orElseGet(() -> new Icd());
-		model.addAttribute("icd", icd);
-		return "icd/add_icd_form";
-	}
-	
-	@PostMapping("/icds")
-	public String saveIcd(
-			@Valid Icd icd
-			,Errors errors
-			,RedirectAttributes redirect
-			,Model model
-			) {
-		if (errors.hasErrors()) {
-			model.addAttribute("uxmessage", new UXMessage("ERROR", "Please check items marked in red."));
-			return "/icds";
-		}
-		icdRepository.save(icd);
-		return "redirect:/icds";
-	}
+    private IcdRepository icdRepository;
+
+    public IcdController (
+            IcdRepository icdRepository
+            ) {
+        this.icdRepository = icdRepository;
+    }
+
+    @GetMapping("/icds")
+    public String listAll(Model model) {
+        List<Icd> icds = this.icdRepository.findAll();
+        model.addAttribute("icds", icds);
+        model.addAttribute(new Icd());
+        return "admin/icd_list";
+    }
+
+    @GetMapping("/add_icd")
+    public String addIcdForm(Model model) {
+        model.addAttribute("icd", new Icd());
+        return "admin/add_icd_form";
+    }
+
+    @GetMapping("edit_icd/{icdId}")
+    public String editIcd(
+            @PathVariable long icdId
+            ,Model model
+            ) {
+        Optional<Icd> oIcd = this.icdRepository.findById(icdId);
+        Icd icd = oIcd.orElseGet(com.cdsi.emr.icd.Icd::new);
+        model.addAttribute("icd", icd);
+        return "icd/add_icd_form";
+    }
+
+    @PostMapping("/icds")
+    public String saveIcd(
+            @Valid Icd icd
+            ,Errors errors
+            ,RedirectAttributes redirect
+            ,Model model
+            ) {
+        if (errors.hasErrors()) {
+            model.addAttribute("uxmessage", new UXMessage("ERROR", "Please check items marked in red."));
+            return "/icds";
+        }
+        this.icdRepository.save(icd);
+        return "redirect:/icds";
+    }
+
+    @GetMapping("/icd/datalist")
+    public ResponseEntity<FlexDatalistResult> flexDatalistForIcd() {
+        List<Icd> result = this.icdRepository.findAll();
+        return ResponseEntity.ok().body(new FlexDatalistResult(result));
+    }
 }
