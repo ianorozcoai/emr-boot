7aa82417107522267a01797bd6e7400eb992546d
Louis Dizon <louis.dizon@gmail.com>
2021-03-14 22:04:49 +0800

    Upload issue fix

--- src/main/java/com/cdsi/emr/EmrApplication.java
+++ src/main/java/com/cdsi/emr/EmrApplication.java
@@ -2,10 +2,21 @@ package com.cdsi.emr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.cdsi.emr.fileupload.FileStorageProperties;
 
 @SpringBootApplication
-public class EmrApplication {
+@EnableConfigurationProperties(FileStorageProperties.class)
+public class EmrApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EmrApplication.class);
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(EmrApplication.class, args);
 	}
